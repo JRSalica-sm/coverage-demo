2774be67d5fa11068100230f0b96654754f41290
JRSalica-sm <jorge.salica@scalemote.co>
2024-01-04 18:33:22 -0300

    Set code coverage config: - Added dependencies. - Added cypress and next config. - Added cypress test setup files. - Added index test and data test attribute to index page. - Updated gitignore

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+// Cypress E2E Test
+describe("Navigation", () => {
+  it("should navigate to the home page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get("[data-cy=welcome-message]").should("be.visible");
+    cy.get("[data-cy=welcome-message]").contains("Get started by editing");
+    cy.get("[data-cy=welcome-message]")
+      .find("code")
+      .contains("src/app/page.tsx");
+  });
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
